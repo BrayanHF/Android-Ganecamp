e9001c99586850f1fbb653adb9caa3521aa285fe
Brayan Hernandez <brayanhernandezfurnieles@gmail.com>
2024-05-17 15:45:25 -0500

    initial commit

---
+++ app/src/main/java/com/ganecamp/ui/main/AnimalsScreen.kt
@@ -0,0 +1,19 @@
+package com.ganecamp.ui.main
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AnimalScreen(viewModel: AnimalsViewModel) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ){
+
+    }
+}
